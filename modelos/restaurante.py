6cc0bea6c6b98c57f8f98342125f32f3c044c34e
renataalineb <renataalinebv@gmail.com>
2024-11-23 16:38:00 -0300

    Importando classe

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,11 +1,24 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+    def __str__(self):
+       return f'{self._nome} | {self.categoria}'
 
-print(restaurantes)
+    @classmethod    
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+     return '☑' if self._ativo else '☐'
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
